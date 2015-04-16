627f5942d73a6f86cabd28c0b16b4dc19c15b1b1
jasmith1125 <jasmith1125@yahoo.com>
2015-04-16 17:21:07 -0400

    still working on password validation

--- js/hw3b.js
+++ js/hw3b.js
@@ -11,7 +11,8 @@ var pass1 = document.forms[0].pwd1;
 var pass2 = document.forms[0].pwd2;
 var passHint1 = document.getElementById("pwd1Hint");
 var passHint2 = document.getElementById("pwd2Hint");
-var myRegEx = /^([a-zA-Z0-9@*#]{8,12})$/;
+var myRegEx = /^([a-zA-Z0-9@*#]{8,15})$/;
+
 pass1.addEventListener("input", function() {
  
 if (myRegEx.test(pass1.value))
@@ -19,10 +20,24 @@ if (myRegEx.test(pass1.value))
         passHint1.style.display = "none"; 
     } else {
         passHint1.style.display = "block";
+        passHint1.innerHTML = "Password must be between 8 and 15 characters"
+    } 
+
+}); // end event listener
+
+pass2.addEventListener("input", function() {
+ 
+if (pass1.value === pass2.value)
+ {
+        passHint2.style.display = "none"; 
+    } else {
+        passHint2.style.display = "block";
+        passHint2.innerHTML = "Passwords must match"
     } 
 
 }); // end event listener
 
 
-
 } // end window onload
+
+// http://www.the-art-of-web.com/javascript/validate-password/
